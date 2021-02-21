a752499a2d17eefd5932fce15e942c9200c373f2
Zzdamon <72194375+Zzdamon@users.noreply.github.com>
2021-02-21 20:53:42 +0200

    added filter buttons

--- admin/src/components/EmployeeList.jsx
+++ admin/src/components/EmployeeList.jsx
@@ -23,10 +23,28 @@ class EmployeeList extends Component {
         this.setState({employees:empSorted})
     }
 
+    filter(value){
+        let empFiltered= this.props.employees;
+        
+        if(value===1){
+            empFiltered=empFiltered.filter((emp)=>emp.salary<2500)
+        }
+
+        if(value===2){
+            empFiltered=empFiltered.filter((emp)=>emp.salary>2500&&emp.salary<4000)
+        }
+
+
+        if(value===3){
+            empFiltered=empFiltered.filter((emp)=>emp.salary>4000)
+        }
+        this.setState({employees:empFiltered})
+
+    }
+
     render() {
         console.log("EmployeeList: ")
         console.log(this.state.employees)
-
 
         return (
             <div>
@@ -36,9 +54,14 @@ class EmployeeList extends Component {
                <button onClick={()=>this.sort("salary")}>Salary</button>
 
                <h4>Filter Salary</h4>
-                <input type="button" name="" value="<2500" id=""/>
-               <button>2500-4000</button>
-               <input type="button" name="" value=">4000" id=""/>
+                <input type="button" name="" value="<2500"
+                    onClick={()=>this.filter(1)} />
+               
+               <button
+                    onClick={()=>this.filter(2)}>2500-4000</button>
+
+               <input type="button" name="" value=">4000"
+                    onClick={()=>this.filter(3)} />
 
 
                </div>
